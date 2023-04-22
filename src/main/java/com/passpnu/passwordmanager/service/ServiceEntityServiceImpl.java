663d99770fde0e7c2e2479f7088171d2acf08906
Zarathustra4 <maxvoloshchuk1@gmail.com>
2023-04-22 21:04:56 +0300

    Objects.requireNonNull added

--- src/main/java/com/passpnu/passwordmanager/service/ServiceEntityServiceImpl.java
+++ src/main/java/com/passpnu/passwordmanager/service/ServiceEntityServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.naming.NameNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -23,9 +24,7 @@ public class ServiceEntityServiceImpl implements ServiceEntityService{
         List<ServiceEntity> serviceEntities = serviceRepository.findAll();
         List<ServiceDto> serviceDtoList = new ArrayList<>();
         for(ServiceEntity serviceEntity : serviceEntities){
-            serviceDtoList.add(
-                    serviceMapper.entityToDto(serviceEntity)
-            );
+            serviceDtoList.add(serviceMapper.entityToDto(serviceEntity));
         }
         return serviceDtoList;
     }
@@ -33,21 +32,16 @@ public class ServiceEntityServiceImpl implements ServiceEntityService{
     @Override
     public ServiceDto postService(ServiceDto service){
         ServiceEntity serviceEntity = serviceMapper.dtoToEntity(service);
-
-        return serviceMapper.entityToDto(
-                serviceRepository.save(serviceEntity)
-        );
+        return serviceMapper.entityToDto(serviceRepository.save(serviceEntity));
     }
 
     @Override
     public ServiceDto putService(ServiceDto service) throws NameNotFoundException {
-        ServiceEntity serviceEntity = serviceRepository.findByDomain(
-                service.getDomain()
+        String domain = service.getDomain();
+        ServiceEntity serviceEntity = Objects.requireNonNull(
+                serviceRepository.findByDomain(domain),
+                "The service is not found by the domain - " + domain
         );
-
-        if(serviceEntity == null){
-            throw new NameNotFoundException("The service is not found");
-        }
 
         serviceEntity.setDescription(service.getDescription());
         serviceEntity.setLogoPath(service.getLogoPath());
@@ -62,12 +56,11 @@ public class ServiceEntityServiceImpl implements ServiceEntityService{
     @Override
     public ServiceDto getById(String id) throws NameNotFoundException{
         Long longId = Long.parseLong(id);
-        Optional<ServiceEntity> serviceEntityOptional = serviceRepository.findById(longId);
-        if(serviceEntityOptional.isEmpty()){
+        Optional<ServiceEntity> serviceEntity = serviceRepository.findById(longId);
+        if(serviceEntity.isEmpty()){
             throw new NameNotFoundException("The service does not exist");
         }
-
-        return serviceMapper.entityToDto(serviceEntityOptional.get());
+        return serviceMapper.entityToDto(serviceEntity.get());
     }
 
     @Override
